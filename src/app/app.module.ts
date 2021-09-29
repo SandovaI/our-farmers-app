808400550fe923f2f9156f72017043b23ce2cb2b
SandovaI <calebsandoval901@gmail.com>
2021-09-28 21:27:35 -0700

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,23 @@ import { PaymentPage } from './payment/payment.page';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Camera } from '@ionic-native/camera/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @NgModule({
   declarations: [AppComponent, PaymentPage],
   entryComponents: [PaymentPage],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent]
+  providers: [
+    StatusBar,
+    File,
+    SplashScreen,
+    Camera,
+    File,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
