fbbeed5d0ed8e94c54c0f15159d448019951e91e
SandovaI <86376236+SandovaI@users.noreply.github.com>
2022-01-05 13:53:50 -0800

    camera

--- src/app/sellerprofile/editprofile/editprofile.page.ts
+++ src/app/sellerprofile/editprofile/editprofile.page.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { UserPhoto, PhotoService } from '../../services/photo.service';
 
 @Component({
   selector: 'app-editprofile',
-  templateUrl: './editprofile.page.html',
-  styleUrls: ['./editprofile.page.scss'],
+  templateUrl: 'editprofile.page.html',
+  styleUrls: ['editprofile.page.scss']
 })
-export class EditprofilePage implements OnInit {
+export class EditprofilePage {
 
-  constructor() { }
+  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.photoService.loadSaved();
   }
 
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+         }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
